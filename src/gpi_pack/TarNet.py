afbc163688e0ef8cdc955edb1ad4377baf2263ae
Kentaro Nakamura <knakamura@g.harvard.edu>
2025-12-02 12:29:20 -0500

    Update version to 0.1.3

--- src/gpi_pack/TarNet.py
+++ src/gpi_pack/TarNet.py
@@ -170,7 +170,6 @@ class TarNet:
             if self.verbose:
                 pbar = tqdm(total=len(self.train_dataloader), desc=f'Training (Epoch {epoch})')
 
-            #Need to double check if the following is correct
             for _, (r, t, y) in enumerate(self.train_dataloader): #r, t, y
                 if self.verbose:
                     pbar.update()
@@ -185,7 +184,7 @@ class TarNet:
                 loss.backward()
                 torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1.0) #gradient clipping
                 self.optim.step()
-                loss_list.append(loss.item() / len(y))
+                loss_list.append(loss.item())
             self.model.eval()
             valid_loss = self.validate_step()
             all_training_loss.append(np.mean(loss_list)); all_valid_loss.append(valid_loss)
@@ -240,7 +239,7 @@ class TarNet:
                     y0_pred = y0_pred,
                     y1_pred = y1_pred,
                 )
-                valid_loss.append(loss / len(y))
+                valid_loss.append(loss)
         self.valid_loss = torch.Tensor(valid_loss).mean()
         return self.valid_loss
 
@@ -265,6 +264,8 @@ class TarNet:
         dataset = TensorDataset(r)
         dataloader  = DataLoader(dataset, batch_size= self.batch_size)
 
+        self.model.eval()
+
         y0_preds = []; y1_preds = []; frs = []
         with torch.no_grad():
             for batch in tqdm(dataloader, total=len(dataloader), desc = 'Predicting'): #r
@@ -279,6 +280,7 @@ class TarNet:
         frs = torch.cat(frs)
 
         return y0_preds, y1_preds, frs
+
 
 def estimate_k_ate(
         R: Union[list, np.ndarray],
@@ -529,233 +531,6 @@ def estimate_k_ate(
 
     return ate_est, se_est
 
-def estimate_k_categorical(
-        R: Union[list, np.ndarray],
-        Y: Union[list, np.ndarray],
-        T: Union[list, np.ndarray],
-        C: Union[list, np.ndarray] = None,
-        formula_C: str = None,
-        data: pd.DataFrame = None,
-        K: int = 2,
-        num_categories: int = 1,
-        valid_perc: float = 0.2,
-        plot_propensity: bool = True,
-        ps_model = SpectralNormClassifier,
-        ps_model_params: dict = {},
-        batch_size: int = 32,
-        nepoch: int = 200,
-        step_size: int = None,
-        lr: float = 2e-5,
-        dropout: float = 0.2,
-        architecture_y: list = [200, 1],
-        architecture_z: list = [2048],
-        trim: list = [0.01, 0.99],
-        bn: bool = False,
-        patience: int = 5,
-        min_delta: float = 0,
-        model_dir: str = None,
-        verbose: bool = True,
-    ) -> tuple[float, float, float]:
-    """
-    Estimate the pseudo outcome prediction of the categorical outputs P(Y = Category | R, T = 1) - P(Y = Category | R, T = 0)
-    using TarNet with k-fold cross-fitting.
-
-    This function trains a TarNet model to estimate potential outcomes under treated (T=1) and untreated (T=0) scenarios, 
-    and then computes the ATE. It uses k-fold cross-fitting, which helps reduce overfitting by estimating the
-    propensity and outcomes on unseen folds.
-
-    Parameters
-    ----------
-    R : list or np.ndarray
-        A list or NumPy array of hidden states extracted from LLM.
-        Shape: (N, d_R) where N is the number of samples and d_R is the dimension of hidden states.
-        You can load the stored hidden states using `load_hiddens` function.
-
-    Y : list or np.ndarray
-        A list or NumPy array of outcomes, shape: (N,).
-
-    T : list or np.ndarray
-        A list or NumPy array of treatments, shape: (N,). Typically binary (0 or 1).
-
-    C : list or np.ndarray, optional
-        A matrix of additional confounders, shape: (N, d_C). If provided, these will be
-        concatenated to R along axis=1. You can pass either this parameter directly
-        or use `formula_c` and `data`.
-
-    formula_c : str, optional
-        A Patsy-style formula (e.g., `"conf1 + conf2"`) that specifies how to build
-        the confounder matrix from a DataFrame. If this is provided, `data` must also be
-        provided, and `C` will be constructed via `dmatrix(formula_c, data)`.
-        Intercept is removed from the design matrix.
-
-    data : pandas.DataFrame, optional
-        The DataFrame containing the columns used in `formula_c`. If `formula_c` is set,
-        this parameter is required. The resulting design matrix is then concatenated
-        to R as additional confounders.
-
-    K : int, default=2
-        Number of cross-fitting folds (K-fold split).
-        
-    num_categories: int, default=1
-        Number of categories for the outcome variable. If the outcome variable is not categorical, set this to 1.
-
-    valid_perc : float, default=0.2
-        Proportion of the training set to use for validation when fitting TarNet in each fold.
-
-    plot_propensity : bool, default=True
-        Whether to plot the propensity score distribution in the console or a graphing interface
-        (implementation-specific).
-
-    ps_model : object, optional
-        A model/classifier used to estimate the propensity score. 
-        By default, we use a neural network with Spectral Normalization (to ensure Lipshitz continuity).
-
-    ps_model_params : dict, optional
-        Hyperparameters for `ps_model`. For example, `{"input_dim": 2048}` if using a custom model
-        requiring an input dimension.
-
-    batch_size : int, default=32
-        Batch size for TarNet training.
-
-    nepoch : int, default=200
-        Number of epochs to train TarNet.
-
-    step_size : int, optional
-        Step size for the learning rate scheduler (if applicable).
-
-    lr : float, default=2e-5
-        Learning rate for TarNet.
-
-    dropout : float, default=0.2
-        Dropout rate for TarNet layers.
-
-    architecture_y : list, default=[200, 1]
-        List specifying the layer sizes for the outcome heads (treatment-specific networks or final layers).
-        For example, [200, 1] means that the outcome model has two hidden layers, the first with 200 units and the second with 1 unit.
-
-    architecture_z : list, default=[2048]
-        List specifying the layer sizes for the deconfounder.
-        For example, [2048, 2048] means that the deconfounder has two hidden layers, each with 2048 units.
-
-    trim : list, default=[0.01, 0.99]
-        Trimming bounds for the propensity score. 
-        Propensity scores outside this range will be replaced with the nearest bound. 
-
-    bn : bool, default=False
-        Whether to apply batch normalization in TarNet.
-
-    patience : int, default=5
-        Patience for early stopping in TarNet training (number of epochs without improvement).
-
-    min_delta : float, default=0
-        Minimum improvement threshold for early stopping.
-
-    model_dir : str, optional
-        Directory path where the model checkpoints might be saved. If provided, the best model will be saved here and loaded for predictions.
-
-    verbose : bool, default=True
-        Whether to print additional information during training.
-
-    Returns
-    -------
-    ate_est : float
-        The estimated Average Treatment Effect.
-
-    sd_est : float
-        An approximate standard error (SE) of the ATE estimate, computed as the standard
-        deviation of the cross-fitting estimates divided by the square root of the total
-        number of estimates.
-    """
-
-    inputs = [R,Y,T]
-    for i, input in enumerate(inputs):
-        if isinstance(input, list):
-            inputs[i] = np.array(input)
-        elif not isinstance(input, np.ndarray):
-            raise ValueError("Input must be either numpy array or list, but not ", type(input))
-    R, Y, T = inputs
-    
-    if num_categories > 1:
-        #check if the outcome is categorical
-        return_probability = True
-    else:
-        return_probability = False
-    
-    if formula_C is not None:
-        if data is None:
-            raise ValueError("If formula_C is provided, data must be provided as well.")
-        
-        formula_C = "-1 + " + formula_C #to remove intercept
-        C = dmatrix(formula_C, data, return_type='dataframe').values
-    
-    if C is not None:
-        C = np.array(C)
-        if C.shape[0] != R.shape[0]:
-            raise ValueError("C and R must have the same number of samples")
-        R = np.concatenate([R, C], axis=1)
-
-    psi = np.empty((len(Y), num_categories))
-    kf = KFold(n_splits=K, shuffle=True)
-
-    for train_index, test_index in kf.split(Y):
-        r_train, r_test = R[train_index], R[test_index]
-        y_train, y_test = Y[train_index], Y[test_index]
-        t_train, t_test = T[train_index], T[test_index]
-        model = TarNet(
-            epochs= nepoch, learning_rate = lr, batch_size= batch_size,
-            architecture_y = architecture_y, architecture_z = architecture_z, dropout=dropout,
-            step_size= step_size, bn=bn,
-            patience= patience, min_delta= min_delta, model_dir=model_dir, 
-            return_probablity=return_probability, verbose=verbose,
-        )
-        model.fit(R = r_train, Y = y_train, T = t_train, valid_perc = valid_perc)
-        y0_pred, y1_pred, fr = model.predict(r_test)
-        
-        if ps_model == SpectralNormClassifier:
-            #Make sure to set the input_dim to the last layer of the shared representation
-            #when using the default ps_model (SpectralNormClassifier)
-            ps_model_params["input_dim"] = architecture_z[-1]
-
-        #train propensity score purely on test data (use two-fold cross fitting)
-        #fitting propensity score
-        index1, index2 = train_test_split(range(len(t_test)), test_size=0.5, random_state=42)
-        ps_model1 = ps_model(**ps_model_params)
-        ps_model2 = ps_model(**ps_model_params)
-    
-        ps_model1.fit(fr[index1].cpu().numpy(), t_test[index1])
-        t_pred2 = ps_model1.predict_proba(fr[index2].cpu().numpy())[:,1]
-    
-        ps_model2.fit(fr[index2].cpu().numpy(), t_test[index2])
-        t_pred1 = ps_model2.predict_proba(fr[index1].cpu().numpy())[:,1]
-    
-        tpreds = np.zeros(len(t_test))
-        tpreds[index1] = t_pred1; tpreds[index2] = t_pred2
-        
-        if plot_propensity:
-            ts = np.zeros(len(t_test))
-            ts[index1] = t_test[index1]; ts[index2] = t_test[index2]
-            _ = plt.hist(tpreds[ts == 1], alpha = 0.5, label = "Treated", density = True)
-            _ = plt.hist(tpreds[ts == 0], alpha = 0.5, label = "Control", density = True)
-            plt.xlabel("Estimated Propensity Score")
-            plt.ylabel("Density")
-            plt.legend(loc = "upper left")
-            plt.show()
-            
-        if trim is not None:
-            tpreds[tpreds < min(trim)] = min(trim)
-            tpreds[tpreds > max(trim)] = max(trim)
-        
-        #estimate pseudo outcomes (predicted probability)
-        for c in range(num_categories):
-            # Compute the pseudo outcome for category c using the original dml_score function
-            psi[test_index, c] = dml_score(t_test, y_test, tpreds, y1_pred[:, c], y0_pred[:, c])
-    
-    ate = np.mean(psi, axis=0)
-    se = np.std(psi, axis=0) / np.sqrt(len(psi))
-    print("ATE:", ate, " /  SE:", se)
-    
-    return ate, se
-
 
 class TarNetHyperparameterTuner:
     '''
@@ -806,6 +581,7 @@ class TarNetHyperparameterTuner:
         bn: list[bool] = [True, False],
         patience_min: int = 5,
         patience_max: int = 20,
+        model_dir: str = None,
     ):
         #loading data
         self.T = T; self.Y = Y
@@ -828,6 +604,7 @@ class TarNetHyperparameterTuner:
         self.dropout = dropout; self.step_size = step_size
         self.architecture_y = architecture_y; self.architecture_z = architecture_z
         self.bn = bn; self.patience_min = patience_min; self.patience_max = patience_max
+        self.model_dir = model_dir
 
     def get_value_or_suggestion(self, name, values, suggest_method):
         if len(values) == 1:
@@ -855,6 +632,8 @@ class TarNetHyperparameterTuner:
             step_size=step_size,
             bn=bn,
             patience = patience,
+            min_delta = 0,
+            model_dir=self.model_dir,
         )
 
         model.fit(self.R, self.Y, self.T, valid_perc= self.valid_perc, plot_loss=False)
